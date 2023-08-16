56db116c378df5eef1d7dfc08fcb8f1f9fd905c1
njwright92 <njwright92@gmail.com>
2023-08-16 15:10:29 -0700

    "comicbot up and running"

--- src/pages/ComicBot.js
+++ src/pages/ComicBot.js
@@ -1,38 +1,54 @@
 import React, { useState } from 'react';
 import Navbar from './components/navbar';
+import axios from 'axios';
 
 const ComicBot = () => {
     const [conversation, setConversation] = useState([]);
     const [userInput, setUserInput] = useState('');
+    const askComicbot = async (prompt) => {
+        try {
+            const res = await axios.post('http://localhost:3001/ask-comicbot/', { prompt });
+            return res.data; // Return the whole data object
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
 
     const handleInputChange = (e) => {
         setUserInput(e.target.value);
     };
 
-    const handleSend = () => {
+    const handleSend = async () => {
         setConversation([...conversation, { from: 'user', text: userInput }]);
-        setUserInput(''); // Clearing the input field
+        setUserInput('');
+
+        try {
+            const botResponses = await askComicbot(userInput); // Get the bot's response directly
+
+            // If you want to use just the first response
+            const botResponse = botResponses[0].generated_text;
+
+            // Append the bot's response to the conversation
+            setConversation(prevConversation => [...prevConversation, { from: 'bot', text: botResponse }]);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
+
     return (
-        <main className="bg-gradient-to-b from-rgb(var(--background-start-rgb)) to-rgb(var(--background-end-rgb)) min-h-screen p-8">
+        <main className="bg-gradient-to-b from-rgb(var(--background-start-rgb)) to-rgb(var(--background-end-rgb)) min-h-screen p-4">
             <Navbar />
-            <h1 className="text-4xl text-white text-center mb-10 glow">ComicBot
-            </h1>
-            <div className="max-w-md mx-auto bg-white p-8 shadow-md rounded-md">
-                <div className="conversation mb-4">
-                    {conversation.map((message, index) => (
-                        <p key={index} className={message.from === 'bot' ? 'bot-message' : 'user-message'}>
-                            {message.text}
-                        </p>
-                    ))}
-                </div>
-                <div className="input-area">
+            <h1 className="text-4xl text-white text-center mb-10 glow">ComicBot</h1>
+            <div className="w-full mx-auto bg-white p-8 shadow-md rounded-md">
+                <div className="input-area flex flex-col items-center">
                     <input
                         type="text"
                         value={userInput}
                         onChange={handleInputChange}
-                        className="p-2 w-full border border-black rounded text-black"
+                        className="p-2 border border-black rounded text-black"
                         placeholder="Write your bit..."
                     />
                     <button
@@ -42,8 +58,21 @@ const ComicBot = () => {
                         Send
                     </button>
                 </div>
+                <div className="w-full mx-auto bg-white p-8 shadow-md rounded-md">
+                    {conversation.map((message, index) => (
+                        <div
+                            key={index}
+                            className={message.from === 'bot' ? 'bot-message-container' : 'user-message-container'}>
+                            <p
+                                className={message.from === 'bot' ? 'bot-message' : 'user-message'}>
+                                {message.text}
+                            </p>
+                            {message.from === 'bot' && <span className="text-black">Bot:..</span>}
+                        </div>
+                    ))}
+                </div>
             </div>
-        </main>
+        </main >
     );
 };
 
