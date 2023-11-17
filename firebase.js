613ade3314ce82f8c26980cf7838a1057329a3e6
njwright92 <njwright92@gmail.com>
2023-11-17 14:27:59 -0800

    "added google analytics"

--- firebase.js
+++ firebase.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, CACHE_SIZE_UNLIMITED, persistentLocalCache } from 'firebase/firestore';
-import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider } from "firebase/auth"
-
+import {
+  getFirestore,
+  CACHE_SIZE_UNLIMITED,
+  persistentLocalCache,
+} from "firebase/firestore";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,7 +19,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,14 +27,16 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
+if (typeof window !== "undefined") {
+  getAnalytics(app);
+}
 
-getFirestore(app,
-  { localCache: persistentLocalCache(/*settings*/{}) });
+getFirestore(app, { localCache: persistentLocalCache(/*settings*/ {}) });
 
 const firestoreDb = getFirestore(app, {
-  cacheSizeBytes: CACHE_SIZE_UNLIMITED
+  cacheSizeBytes: CACHE_SIZE_UNLIMITED,
 });
 
 setPersistence(auth, browserLocalPersistence);
 
-export { app, db, auth, firestoreDb, provider }
+export { app, db, auth, firestoreDb, provider };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,26 @@
-import("@/styles/globals.css");
-import("aos/dist/aos.css");
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import "@/styles/globals.css";
+import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      const analytics = getAnalytics();
+      logEvent(analytics, "page_view", {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
